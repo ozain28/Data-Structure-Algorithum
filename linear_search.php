a6b51f3701a0fdb707c074eac6445cc52ff552c1
ozain28 <babubabuddin95@gmail.com>
2026-03-26 02:46:06 +0500

    added linear search

---
+++ linear_search.php
@@ -0,0 +1,13 @@
+<!-- // public function lowercase(){
+//     $arr = ['ozain' , 'Shazaib' ,'SHAZAIN'];
+//     $target = 'anus';
+//     $count = 0 ;
+
+//     foreach($arr as $index => $ar){
+//         if(strtolower($ar) == strtolower($target)){
+//             $count++;
+//             echo"mill gya $index mill gya $target mill gya $count";
+
+//         }
+//     }
+//     } -->
