dfddd5e358cf366d0d50ba63aa3d65c007ad258c
ozain28 <babubabuddin95@gmail.com>
2026-03-26 07:02:53 +0500

    binary search

---
+++ binary-search.php
@@ -0,0 +1,133 @@
+ //public function lowercase(){
+//        $arr = [1,2,3,4,5,6,7,8,9];
+//         $target = 2 ;
+
+//        $low = 0; 
+//        $high = count($arr) -1;
+//        //echo "$high";
+       
+//        while($low <= $high){
+
+//               $mid = floor(($low + $high)/ 2);
+
+//               if($arr[$mid] == $target){
+//                      echo " mill gya $mid ";
+//                      break;
+//               }
+//               elseif($arr[$mid] < $target){
+//                      $low = $mid +1 ;
+//                   //echo " mill gya $mid ";
+//               }
+//               else{
+//                      $high = $mid -1;
+//                   //echo " mill gya $mid ";
+//               }
+              
+              
+              
+   <--------------------------------------------------------.>           
+
+//        }
+// $arr = [1,2,3,4,5,6,7,8,9];
+// $target = 2;
+
+// $low = 0;
+// $high = count($arr) - 1;
+
+// while($low <= $high){
+
+//     $mid = floor(($low + $high) / 2);
+
+//     if($arr[$mid] == $target){
+//         echo "mil gaya index $target";
+//         break;
+//     }
+
+//     elseif($arr[$mid] < $target){
+//         $low = $mid + 1;
+//     }
+
+//     else{
+//         $high = $mid - 1;
+//     }
+// }
+
+
+
+
+//  }
+
+
+
+ //}
+ <-------------------------------------------------------------.>
+//  public function lowercase(){
+//     $arr= [1,2,3,4,5,6,7,8,9];
+//     $target= 2;
+//     $low = 0 ;
+//     $high = count($arr) -1 ;
+//     while($low <= $high){
+//         $mid =  floor(($low + $high)) / 2;
+//          if($mid == $target){
+//             echo "mill gya $target";
+//             break;         
+//          }
+//         elseif($arr[$mid] > $target){
+//             $high = $mid +1;
+//           }
+//           else{
+//             $low = $mid -1;
+//           }
+
+
+
+//     }
+//  }
+<-------------------------------------------------------------------------------.>
+// public function lowercase(){
+//     $arr = [1,2,3,4,5,6,7,8,9];
+//     $target = 3;
+
+//     $low = 0 ;
+//     $high = count($arr) -1 ;
+
+//     while($low <= $high){
+//     $mid = floor(($low + $high)) /2 ;
+//         if($mid == $target){
+//             echo " mill gya $target";
+//             break;
+
+//         }
+//         elseif($arr[$mid] > $target){
+//             $high = $mid +1;
+
+
+//         }
+//         else{
+//             $low = $mid -1;
+//         }
+        
+//     }
+// }
+<-------------------------------------------------------------------------------.--.>
+// public function lowercase(){
+//     $arr = [1,2,3,4,5,6,7,8,9];
+//     $target = 2 ;
+//     $low = 0 ; 
+//     $high = count($arr) -1 ;
+//     while($low <= $high){
+//         $mid = floor($low + $high) /2;
+//      if($mid == $target){
+//         echo "mill gya $target";
+//         break;
+//      }
+//      elseif($arr[$mid] > $target){
+//         $high = $mid +1;
+
+//      }
+// else {
+//     $low = $mid -1 ;
+// }
+
+//     } 
+// }
