7cac752f541a841a3adcaf281f591ad8a0acb64b
ozain28 <babubabuddin95@gmail.com>
2026-03-26 06:57:26 +0500

    after update

--- linear_search.php
+++ linear_search.php
@@ -11,3 +11,23 @@
 //         }
 //     }
 //     } -->
+<----------------------------------------.>
+// public function lowercase(){
+//     $arr = ['ozain' , 'Shazaib' ,'SHAZAIN'];
+//     $target = 'anus';
+//     $count = 0 ;
+
+//     foreach($arr as $index => $ar){
+//         if(strtolower($ar) == strtolower($target)){
+//             $count++;
+//             echo"mill gya $index mill gya $target mill gya $count";
+
+//         }
+//     }
+//     }
+    // } 
+    // if($count == 0 ){
+    //     echo "kuch nhi mila ";
+        
+    // }
+    //   return $count ;
