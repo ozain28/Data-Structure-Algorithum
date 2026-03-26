4f8e36805782929102e0bd9623e25595cba7938c
ozain28 <babubabuddin95@gmail.com>
2026-03-26 06:40:05 +0500

    lenear_searchhh

---
+++ lenear_search.php
@@ -0,0 +1,162 @@
+//     function lenearfuntion($arr , $target){
+//         foreach($arr as $value){
+//             if($value == $target){
+//                 echo "mill gya "
+
+//             }
+//             else{
+//                 echo "nhi mila "
+//             }
+//         }
+<---------------------------------------------->
+//     function lenearmultiple($arr , $target){
+//         $count = 0;
+//         foreach($arr as $index => $value){
+//             if($value == $target){
+//                 $count++;
+//                 echo "mill gya $index \n";
+//             }
+//          }
+//          echo"total count found : $count";
+<-------------------------------------------------->
+//          function lenearmulticase($arr , $target){
+//             $count = 0 ;
+//             $lowcase = strtolower($target);
+//             foreach($arr as $index => $value){
+//                 if(strtolower($value) == $lowcase){
+//                     $count++;
+//                     echo "mill gya : $value  or  : $index \n";
+
+//                 }
+
+//             }
+//                     echo "total $target found : $count";
+
+//          }
+<--------------------------------------------------------------------->
+//     function lenearsearch($arr, $target){
+//         $count = 0 ;
+//         foreach($arr as $value){
+//            if($value == $target){
+//             $count++;
+//             echo "mill gya : $count";
+//            }
+//         }
+//           echo "nhi mila";
+<----------------------------------------------------------------->
+//  function lrnearsearch($arr , $target){
+//     $count = 0 ;
+//     foreach($arr as $index => $value){
+//         if($value == $target){
+//            $count++;
+//            echo "mill gya $value or $index \n"; 
+//         }
+
+//     }
+// echo "total found $count ";
+//  }
+<------------------------------------------------------->
+//  function lowercase($arr , $target ){
+//     $count = 0;
+//     $lowercase = strtolower($target);
+//    foreach($arr as $index => $value ){
+//     if($value == $lowercase){
+//         $count++;
+//     echo "mill gya $value or $index \n";
+
+// }
+   
+
+//    }
+//   echo "total count : $count";
+
+
+
+//  }
+<----------------------------------------------------------->
+// function lowercase($users , $target){
+//     $count = 0; 
+//     $case = strtolower($target);
+//     foreach($users as $index => $case){
+//         if($case == $target){
+//             $count++;
+//         echo "mill gya : $target";
+//         //echo "";
+//          }
+
+//         if($count > 0 ){
+//          echo "total count found : $count";
+
+//         }
+
+//     }
+
+
+// }
+
+
+
+//     }
+<------------------------------------------------------------>
+// function lenearsearch($users , $target){
+//     $count = 0;
+//     foreach($users as $index => $value){
+//         if ($value == $target){
+//             $count++;
+//             echo "mill gya $count and position $index \n";
+//         }
+
+//     }
+// echo "mill gya $target ";
+// }
+<--------------------------------------------------------------------->
+// function lowercase($users , $target){
+//     $count = 0 ;
+//     $lower = strtolower($target);
+//     foreach($lower as $index => $low){
+//         if($low == $users){
+//             $count++;
+
+//         }
+
+//     }
+
+// }
+<------------------------------------------------------------>
+// function lowercase($users , $target){
+//     $count = 0;
+//     $lowcase  = strtolower($target);
+//     foreach($users as $index => $low){
+//         if(strtolower($low) == $lowcase){
+//             $count++;
+//             echo "mill gya : $low at position : $index \n";
+//             }
+            
+         
+//     }   
+// }
+<---------------------------------------------------------------->
+// function lowercase($users , $target){
+//     $found = false;
+//     foreach($users as $index => $user){
+//         if(strtolower($user) == strtolower($target)){
+//            echo "mill gya : $user or ya $index \n";
+//            $found = true;
+//            break;
+
+//         }
+
+//     }
+
+// }
+<---------------------------------------------------------------->
+//  function lowercase($users , $target){
+//     $count = 0 ;
+//     foreach($users as $index => $user){
+//         if(strtolower($user) == strtolower($target)){
+//             $count++;
+//             echo "mill gya index : $index mill gya name $target\n";
+//             break;
+//         }
+
+//     }
